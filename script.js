93763a57f14d69ea21356a8176710f714c42e666
Jagadeesh3996 <jagadeeshau032@gmail.com>
2023-09-19 22:09:28 +0530

    script page updated

--- script.js
+++ script.js
@@ -1,139 +1,162 @@
 
+function oddEven(){                                       // function starts
 
-function oddEven() {
+    const num = Number(input.value);                      // input is converted to number
 
-    let input = document.getElementById('inputBx').value;
-    let number = Number(input);
-
-    if(number == ''){
+    if(num == ''){                                       // checking the input is empty or not
         alert("Enter Something !");
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number != input){
+    else if(num != input.value){                         // checking the input is number or not
         alert("Enter a Number !");
-        document.getElementById('inputBx').value = "";
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number % 2 == 0){
-        document.getElementById('outputBx').innerHTML = number + " is Even Number";
-        document.getElementById('inputBx').value = "";
+    else if(num % 2 == 0){                              // checking the input is Even or not
+        output.innerHTML = num + " is Even Number";
+        input.value = "";
     }
     else{
-        document.getElementById('outputBx').innerHTML = number + " is Odd Number";
-        document.getElementById('inputBx').value = "";
+        output.innerHTML = num + " is Odd Number";
+        input.value = "";
     }
-}
+}                                                       // function ends
 
 
-function isPrime(){
 
-    const input = document.getElementById('inputBx').value;
-    const number = Number(input);
+function isPrime(){                                     // function starts
+
+    const num = Number(input.value);                     // input is converted to number
     let isPrime = true;
 
-    if (number <= 1) {
+    if(num <= 1){                                       // checking number is greater than 1
         isPrime = false;
     }
-    else {
-        for (let i = 2; i < number; i++) {
-          if (number % i === 0) {
+    else{
+        for(let i = 2; i <= Math.sqrt(num); i++){        // checking number is Prime or not
+            if(num % i == 0){
                 isPrime = false;
                 break;
             }
         }
     }
 
-    if(number == ''){
+    if(num == ''){                                       // checking the input is empty or not
         alert("Enter Something !");
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number != input){
+    else if(num != input.value){                         // checking the input is number or not
         alert("Enter a Number !");
-        document.getElementById('inputBx').value = "";
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(isPrime){
-        document.getElementById('outputBx').innerHTML = input + " is Prime Number";
-        document.getElementById('inputBx').value = "";
+    else if(num <= 0){                                  // checking the input is positive or not
+        alert("Enter a Positive Number !");
+        input.value = "";
+        output.innerHTML = "";
+    }
+    else if(isPrime){                                       // checking true or false
+        output.innerHTML = num + " is Prime Number";
+        input.value = "";
     }
     else{
-        document.getElementById('outputBx').innerHTML = input + " is Composite Number";
-        document.getElementById('inputBx').value = "";
+        output.innerHTML = num + " is Composite Number";
+        input.value = "";
     }
+}                                                       // function ends
 
-}
 
 
-function factorial(){
+function factorial(){                                       // function starts
 
-    const input = document.getElementById('inputBx').value;
-    const number = Number(input);
-    var fact = 1;
+    const num = Number(input.value);                      // input is converted to number
+    let fact = 1;
 
-    if(number == ''){
+    if(num == ''){                                       // checking the input is empty or not
         alert("Enter Something !");
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number != input){
+    else if(num != input.value){                         // checking the input is number or not
         alert("Enter a Number !");
-        document.getElementById('inputBx').value = "";
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else {
-        for (var i=2; i<=number; i++){
+    else if(num <= 0){                                  // checking the input is positive or not
+        alert("Enter a Positive Number !");
+        input.value = "";
+        output.innerHTML = "";
+    }
+    else{    
+        for (var i=2; i<=num; i++){                        // finding factorial for input
             fact *=i ;
         }
-        document.getElementById('outputBx').innerHTML = "The Factorial of "+ number +" is "+ fact;
-        document.getElementById('inputBx').value = "";
-    }
-}
+        output.innerHTML = "The Factorial of "+ num +" is "+ fact;
+        input.value = "";
+    }   
+}                                                       // function ends
 
 
-function fibonacci(){
 
-    const input = document.getElementById('inputBx').value;
-    const number = Number(input);
-    var fibo = [0, 1];
+function fibonacci(){                                       // function starts
 
-    if(number == ''){
+    const num = Number(input.value);                      // input is converted to number
+    let fibo = [0, 1];
+
+    if(num == ''){                                       // checking the input is empty or not
         alert("Enter Something !");
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number != input){
+    else if(num != input.value){                         // checking the input is number or not
         alert("Enter a Number !");
-        document.getElementById('inputBx').value = "";
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else if(number < 2){
-        document.getElementById('outputBx').innerHTML = "The Fibonacci Sequence of "+ number +" is "+ fibo[number - 1];
-        document.getElementById('inputBx').value = "";
+    else if(num <= 0){                                  // checking the input is positive or not
+        alert("Enter a Positive Number !");
+        input.value = "";
+        output.innerHTML = "";
     }
-    else{
-        for (var i=2; i<number; i++){
+    else if(num < 2){                                  // checking the input is lesser than 2
+        output.innerHTML = "The Fibonacci Sequence of "+ num +" is "+ fibo[num - 1];
+        input.value = "";
+    }
+    else{    
+        for (var i=2; i<num; i++){                        // finding fibonacci for input
             fibo[i] = fibo[i - 1] + fibo[i - 2];
         }
-        document.getElementById('outputBx').innerHTML = "The Fibonacci Sequence of "+ number +" is "+ fibo.join(", ");
-        document.getElementById('inputBx').value = "";
-    }
-}
+        output.innerHTML = "The Fibonacci Sequence of "+ num +" is "+ fibo.join(", ");
+        input.value = "";
+    }     
+}                                                       // function ends
 
 
-function palindrome(){
 
-    const input = document.getElementById('inputBx').value;
-    var revInput = input.split("").reverse().join("");
+function palindrome(){                                       // function starts
 
-    if(input == ''){
+    let inputSt = input.value;
+    var revInput = inputSt.split("").reverse().join("");       // Reverse the input
+
+    if(inputSt == ''){                                       // checking the input is empty or not
         alert("Enter Something !");
-        document.getElementById('outputBx').innerHTML = "";
+        input.value = "";
+        output.innerHTML = "";
     }
-    else {
-        if (input === revInput) {
-            document.getElementById('outputBx').innerHTML = input +" is a Palindrome";
-            document.getElementById('inputBx').value = "";
-          } else {
-            document.getElementById('outputBx').innerHTML = input +" is not a Palindrome";
-            document.getElementById('inputBx').value = "";
-          }
+    else if (inputSt === revInput){                              // checking the input is Palindrome or not
+        output.innerHTML = inputSt +" is a Palindrome";
+        input.value = "";
     }
-}
+    else{
+        output.innerHTML = inputSt +" is not a Palindrome";
+        input.value = "";
+    }
+}                                                       // function ends
+
+
+
+
+var input = document.getElementById('inputBx');         // assigning input to variable using Id and DOM
+var output = document.getElementById('outputBx');       // assigning output to variable using Id and DOM
